6a29721c6f00b6afd2e8a8a41e01eb24563c2ba9
Vinayak Ratan <59795932+vinayakr6@users.noreply.github.com>
2024-08-29 07:47:05 +0000

    min,max,sum,any,all,key

--- Start/Ch_1/minmax.py
+++ Start/Ch_1/minmax.py
@@ -9,14 +9,32 @@ strings = ["one", "three", "five", "seven", "eleven", "eighteen"]
 
 
 # TODO: The min() function finds the minimum value
-
+print(f'The Minimum Value is: {min(values)}')
+print(f'The Minimum Value is: {min(strings)}')
 
 # TODO: The max() function finds the maximum value
-
+print(f'The Maximum Value is: {max(values)}')
+print(f'The Maximum Value is: {max(strings)}')
 
 # TODO: define a custom "key" function to extract a data field
-
+print(f'The Minimum Value is: {min(strings, key = len)}')
+print(f'The Maximum Value is: {max(strings, key = len)}')
 
 # TODO: open the data file and load the JSON
-# with open("../../30DayQuakes.json", "r") as datafile:
-#     data = json.load(datafile)
+with open("../../30DayQuakes.json", "r") as datafile:
+    data = json.load(datafile)
+
+print(data["metadata"]['title'])
+print(len(data['features']))
+
+# define a custom "key" function to extract a data field
+def getmag(dataitem):
+    magnitude = dataitem["properties"]["mag"]
+    if (magnitude is None):
+        magnitude = 0
+    return float(magnitude)
+
+print(data["metadata"]["title"])
+print(len(data["features"]))
+print(min(data["features"], key=getmag))
+print(max(data["features"], key=getmag))
